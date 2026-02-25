95f09aa8c9aec2494fc95f4bc2dfeed6651ce9fa
clarkkuang <xkuang30@gatech.edu>
2026-02-24 17:54:27 -0800

    Initial Commit

---
+++ LeaveNow/LeaveNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaveNowApp.swift
+//  LeaveNow
+//
+//  Created by Clark Kuang on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct LeaveNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
